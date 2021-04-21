b20108725edf039fa417e9b868bc38ac917753da
Brand0nKelly <c19428962@mytudublin.ie>
2021-04-21 16:54:49 +0100

    Note

---
+++ java/src/ie/tudublin/Note.java
@@ -0,0 +1,25 @@
+package ie.tudublin;
+
+
+public class Note 
+{
+    private char note;
+    private int duration;
+
+public Note(char note, int duration) 
+{
+    this.note = note;
+    this.duration = duration;
+}
+
+
+
+@Override
+public String toString() 
+{
+    return "Note [duration=" + duration + ", note=" + note + "]";
+}
+
+
+
+}
